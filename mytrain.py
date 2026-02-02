f18772998da91cf2c88eded9d42849e7e2711a1d
UltralyticsAssistant <web@ultralytics.com>
2026-02-02 09:52:10 +0000

    Auto-format by https://ultralytics.com/actions

--- mytrain.py
+++ mytrain.py
@@ -1,10 +1,9 @@
 from ultralytics import YOLO
 
 if __name__ == "__main__":
-
     model = YOLO("yolov8n-pose.pt")
     model.train(
-        data='coco8-pose.yaml',
+        data="coco8-pose.yaml",
         epochs=100,
         imgsz=640,
-        )
+    )
--- 训练脚本备份.py
+++ 训练脚本备份.py
@@ -2,11 +2,11 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     model = YOLO(r"yolo11n.pt")
-    model.train(                #我们要开始训练
-        data=r"coco8.yaml",     #打算训练coco8这个训练集
-        epochs=30,              #训练**轮
-        imgsz=640,              #全称image size，即图片尺寸
-        batch=2,                #批
-        cache=False,            #缓存
-        workers=0,              #进程
+    model.train(  # 我们要开始训练
+        data=r"coco8.yaml",  # 打算训练coco8这个训练集
+        epochs=30,  # 训练**轮
+        imgsz=640,  # 全称image size，即图片尺寸
+        batch=2,  # 批
+        cache=False,  # 缓存
+        workers=0,  # 进程
     )
